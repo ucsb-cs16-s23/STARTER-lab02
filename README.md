e561793366866ee891bb5cb22b06ae7be9dccf04
rutvikjha <56047961+rutvikjha@users.noreply.github.com>
2023-04-19 15:49:13 -0700

    uploading starter code

---
+++ README.md
@@ -0,0 +1,3 @@
+# STARTER-lab02
+
+https://ucsb-cs16.github.io/s23/labs/lab02/
---
+++ sumOfArray.cpp
@@ -0,0 +1,6 @@
+#include "arrayFuncs.h"
+
+int sumOfArray(int a[], int size) {
+  // put in your code here
+  return 0;
+}
